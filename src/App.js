68c9ea18a38e5cab471950630037f67a38c97f22
Rene C <lilymist2@gmail.com>
2021-03-04 01:38:31 -0500

    final commit, I will become a React Pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mistyjokes/CreateReact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out!
         </a>
       </header>
     </div>
